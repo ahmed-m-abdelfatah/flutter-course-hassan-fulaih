26f392a2eed362f64d7f44ac2e5ff4a3c4249f3b
ahmed-m-abdelfatah <35312200+ahmed-m-abdelfatah@users.noreply.github.com>
2021-10-13 15:30:42 +0200

    Delete 02_meal_app directory

--- 02_meal_app/lib/shared/components/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-BorderRadius myBorderRadius({double num = 15.0}) => BorderRadius.circular(num);
-
-EdgeInsets myEdgeInsets({double num = 15.0}) => EdgeInsets.all(num);
-
-TextStyle titleStyle(BuildContext context) => Theme.of(context).textTheme.headline6!;
-// ! cuz iam sure it will not be null
